b20cb334d4ca02f497369aef63b122543959a9a9
Surya-mazumder9734 <Surya.mazumder@ibm.com>
2021-04-12 12:37:34 +0530

    hello

--- src/main/java/com/example/demo/EurekaProjectGitApplication.java
+++ src/main/java/com/example/demo/EurekaProjectGitApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaProjectGitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(EurekaProjectGitApplication.class, args);
 	}
 
--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		//hello
 		return application.sources(EurekaProjectGitApplication.class);
 	}
 
